112c459c0af60857792c0814dfcc86bd58463c2d
Shahriyar Nasir <snasir@stripe.com>
2023-07-20 19:10:32 -0400

    Fix type errors

--- utils/jwt_encode_decode.ts
+++ utils/jwt_encode_decode.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error TS(7016): Could not find a declaration file for module 'json... Remove this comment to see the full error message
 import jwt from 'jsonwebtoken';
 
 export function encode(payload: any) {
