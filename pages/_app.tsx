8e703001b5f8d9bf3f71d9441ef4c8e3d7051604
Shahriyar Nasir <snasir@stripe.com>
2023-07-22 13:26:52 -0400

    Configure for mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,42 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 import {AppProps, AppContext} from 'next/app';
+import Head from 'next/head';
+import {ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import {CacheProvider, EmotionCache} from '@emotion/react';
 import {decode} from '../utils/jwt_encode_decode';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
 
 import {parse} from 'cookie';
 import * as React from 'react';
 
-type HomeMadeHamProps = AppProps;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-export default function HomeMadeHam({Component, pageProps}: HomeMadeHamProps) {
+interface HomeMadeHamProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function HomeMadeHam({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: HomeMadeHamProps) {
   return (
-    <Layout {...pageProps}>
-      <Component {...pageProps} />
-    </Layout>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Layout {...pageProps}>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
