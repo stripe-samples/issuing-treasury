c1a745b3fe9dde111f6782e0459f83a42101619b
Shahriyar Nasir <snasir@stripe.com>
2023-07-23 19:12:40 -0400

    Update latest transactions table

--- src/sections/overview/overview-latest-transactions.tsx
+++ src/sections/overview/overview-latest-transactions.tsx
@@ -1,3 +1,4 @@
+import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
 import ArrowRightIcon from "@heroicons/react/24/solid/ArrowRightIcon";
 import {
   Box,
@@ -6,12 +7,15 @@ import {
   CardActions,
   CardHeader,
   Divider,
+  Link,
+  Stack,
   SvgIcon,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 import { format } from "date-fns";
 
@@ -57,36 +61,22 @@ export const OverviewLatestTransactions = (props: {
                   <TableCell sx={{ textTransform: "uppercase" }}>
                     {transaction.flow_details[transaction.flow_type]
                       .hosted_regulatory_receipt_url ? (
-                      <span className="text-gray-500 font-medium flex justify-between">
-                        {transaction.flow_type}
-                        <a
+                      <Stack direction="row" spacing={1}>
+                        <Typography>{transaction.flow_type}</Typography>
+                        <Link
                           href={
                             transaction.flow_details[transaction.flow_type]
                               .hosted_regulatory_receipt_url
                           }
                           target="_blank"
-                          rel="noreferrer"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-5 h-5"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-                            />
-                          </svg>
-                        </a>
-                      </span>
+                          <SvgIcon>
+                            <DocumentArrowDownIcon />
+                          </SvgIcon>
+                        </Link>
+                      </Stack>
                     ) : (
-                      <span className="text-gray-500 font-medium flex">
-                        {transaction.flow_type}
-                      </span>
+                      <Typography>{transaction.flow_type}</Typography>
                     )}
                   </TableCell>
                   <TableCell>
