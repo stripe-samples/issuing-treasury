8089ee1947ca7bc98729d309abfd794ea266149a
Shahriyar Nasir <snasir@stripe.com>
2023-07-23 20:02:33 -0400

    Fix type errors

--- src/utils/stripe_helpers.ts
+++ src/utils/stripe_helpers.ts
@@ -1,6 +1,16 @@
-import Stripe from "stripe";
+import { Stripe } from "stripe";
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const API_VERSION = "2022-11-15";
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error("Stripe Secret Key is not set in environment variables.");
+}
+
+const stripe = new Stripe(stripeSecretKey, {
+  apiVersion: API_VERSION,
+});
 
 export async function getFinancialAccountTransactions(StripeAccountID: any) {
   const financialAccounts = await stripe.treasury.financialAccounts.list({
