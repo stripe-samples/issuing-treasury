cd951b2248081e6507ae6d21b2a5df272ad119be
Shahriyar Nasir <snasir@stripe.com>
2023-08-01 12:26:33 -0400

    Capture the destination address, validate it, and move to next form

--- src/sections/financial-account/send-money-wizard-dialog.tsx
+++ src/sections/financial-account/send-money-wizard-dialog.tsx
@@ -11,6 +11,8 @@ import {
   Radio,
   RadioGroup,
   FormHelperText,
+  TextField,
+  Grid,
 } from "@mui/material";
 import { useMachine } from "@xstate/react";
 import {
@@ -21,7 +23,7 @@ import {
   FormikValues,
   ErrorMessage,
 } from "formik";
-import React, { RefObject, useRef } from "react";
+import React, { RefObject, use, useRef, useState } from "react";
 import * as Yup from "yup";
 
 import stateMachine from "src/sections/financial-account/send-money-wizard-state-machine";
@@ -29,9 +31,11 @@ import stateMachine from "src/sections/financial-account/send-money-wizard-state
 const SelectingNetworkForm = ({
   formRef,
   onFormSubmit,
+  setNetwork,
 }: {
   formRef: RefObject<FormikProps<FormikValues>>;
   onFormSubmit: () => void;
+  setNetwork: (network: string) => void;
 }) => {
   const initialValues = {
     network: "",
@@ -46,6 +50,7 @@ const SelectingNetworkForm = ({
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={async (values, { setSubmitting }) => {
+        setNetwork(values.network);
         onFormSubmit();
       }}
     >
@@ -84,6 +89,152 @@ const SelectingNetworkForm = ({
   );
 };
 
+const CollectingDestinationAddressForm = ({
+  formRef,
+  onFormSubmit,
+  network,
+}: {
+  formRef: RefObject<FormikProps<FormikValues>>;
+  onFormSubmit: () => void;
+  network: string;
+}) => {
+  const initialValues = {
+    name: "",
+    routing_number: "",
+    account_number: "",
+    amount: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    routing_number: Yup.string().required("Routing Number is required"),
+    account_number: Yup.string().required("Account Number is required"),
+    amount: Yup.number().required("Amount is required").min(0),
+  });
+
+  return (
+    <Formik
+      innerRef={formRef}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={async (values, { setSubmitting }) => {
+        onFormSubmit();
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Field
+                as={TextField}
+                name="name"
+                required
+                label="Name"
+                fullWidth
+                error={touched.name && Boolean(errors.name)}
+                helperText={touched.name && errors.name}
+              />
+            </Grid>
+            {network === "us_domestic_wire" && (
+              <>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    required
+                    label="Street address"
+                    name="address1"
+                    error={touched.address1 && Boolean(errors.address1)}
+                    helperText={touched.address1 && errors.address1}
+                  />
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    required
+                    label="City"
+                    name="city"
+                    error={touched.city && Boolean(errors.city)}
+                    helperText={touched.city && errors.city}
+                  />
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    required
+                    label="State"
+                    name="state"
+                    error={touched.state && Boolean(errors.state)}
+                    helperText={touched.state && errors.state}
+                  />
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    required
+                    label="ZIP / Postal code"
+                    name="postalCode"
+                    error={touched.postalCode && Boolean(errors.postalCode)}
+                    helperText={touched.postalCode && errors.postalCode}
+                  />
+                </Grid>
+              </>
+            )}
+            <Grid item xs={12} md={6}>
+              <Field
+                as={TextField}
+                name="routing_number"
+                required
+                label="Routing Number"
+                fullWidth
+                error={touched.routing_number && Boolean(errors.routing_number)}
+                helperText={touched.routing_number && errors.routing_number}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Field
+                as={TextField}
+                name="account_number"
+                required
+                label="Account Number"
+                fullWidth
+                error={touched.account_number && Boolean(errors.account_number)}
+                helperText={touched.account_number && errors.account_number}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                as={TextField}
+                name="amount"
+                type="number"
+                required
+                label="Amount"
+                fullWidth
+                error={touched.amount && Boolean(errors.amount)}
+                helperText={touched.amount && errors.amount}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                as={TextField}
+                name="account_notes"
+                type="number"
+                label="Account Notes (Optional)"
+                fullWidth
+                error={touched.account_notes && Boolean(errors.account_notes)}
+                helperText={touched.account_notes && errors.account_notes}
+              />
+            </Grid>
+          </Grid>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
 const SendMoneyWizardDialog = () => {
   const [showModal, setShowModal] = React.useState(false);
   const [current, send] = useMachine(stateMachine);
@@ -99,8 +250,13 @@ const SendMoneyWizardDialog = () => {
     send("BACK");
   };
 
+  const [network, setNetwork] = useState("");
+
   const selectingNetworkFormRef = useRef<FormikProps<FormikValues>>(null);
-  const handleSubmitForm = () => {
+  const collectingDestinationAddressFormRef =
+    useRef<FormikProps<FormikValues>>(null);
+  const confirmingTransferFormRef = useRef<FormikProps<FormikValues>>(null);
+  const handleSubmitSelectingNetworkForm = () => {
     const selectingNetworkForm = selectingNetworkFormRef.current;
     if (selectingNetworkForm) {
       selectingNetworkForm.submitForm();
@@ -120,12 +276,19 @@ const SendMoneyWizardDialog = () => {
             <SelectingNetworkForm
               formRef={selectingNetworkFormRef}
               onFormSubmit={handleNext}
+              setNetwork={setNetwork}
             />
           )}
           {current.matches("collectingDestinationAddress") && (
-            <div>collectingDestinationAddress</div>
+            <CollectingDestinationAddressForm
+              formRef={collectingDestinationAddressFormRef}
+              onFormSubmit={handleNext}
+              network={network}
+            />
           )}
-          {current.matches("confirmingTransfer") && <div>selectingNetwork</div>}
+          {current.matches("confirmingTransfer") && (
+            <div>confirmingTransfer</div>
+          )}
         </DialogContent>
         <Divider />
         <DialogActions>
@@ -134,7 +297,7 @@ const SendMoneyWizardDialog = () => {
               <Button id="foo" onClick={handleNext} variant="contained">
                 Next
               </Button>
-              <Button onClick={handleSubmitForm}>Submit</Button>
+              <Button onClick={handleSubmitSelectingNetworkForm}>Submit</Button>
             </div>
           )}
           {current.matches("collectingDestinationAddress") && (
