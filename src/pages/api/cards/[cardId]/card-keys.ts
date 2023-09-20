bfc7b60fbfe3e6d4d7ad472a581d68fa6ae453db
Shahriyar Nasir <snasir@stripe.com>
2023-09-20 10:42:38 -0400

    Update the Stripe SDK so that we can set the Issuing Elements nonce

--- src/pages/api/cards/[cardId]/card-keys.ts
+++ src/pages/api/cards/[cardId]/card-keys.ts
@@ -37,9 +37,7 @@ const createCardKey = async (req: NextApiRequest, res: NextApiResponse) => {
   const stripe = stripeClient();
   const ephemeralKey = await stripe.ephemeralKeys.create(
     {
-      // TODO: Fix the defect upstream in Issuing Elements causing `nonce` to not be accepted so that we can uncomment
-      // this line and add `nonce` to the request body for creating the ephemeral key.
-      // nonce: nonce,
+      nonce: nonce,
       issuing_card: cardId,
     },
     {
