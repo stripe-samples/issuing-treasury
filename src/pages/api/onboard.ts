a0de4d2f800c3317abe029da2857ecd2a78eeb2b
Edd Morgan <eddm@me.com>
2024-01-24 17:57:52 +0000

    inline some usecase checks
    for making codegen a bit easier in the future

--- src/pages/api/onboard.ts
+++ src/pages/api/onboard.ts
@@ -20,17 +20,6 @@ const onboard = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, stripeAccount, country, useCase } = session;
   const { accountId, platform } = stripeAccount;
 
-  // Embedded Finance is a full financial services stack for your users:
-  // accounts[0] with Treasury to store and send funds, with cards[1] with
-  // Issuing for spending.
-  // This is different from the Expense Management example, where you
-  // top up balances[2] to fund spend on Issuing cards.
-  //
-  // [0] https://stripe.com/docs/treasury/account-management/financial-accounts
-  // [1] https://stripe.com/docs/issuing/how-issuing-works
-  // [2] https://stripe.com/docs/issuing/adding-funds-to-your-card-program
-  const useTreasury = useCase == UseCase.EmbeddedFinance;
-
   const {
     businessName,
     skipOnboarding,
@@ -112,7 +101,16 @@ const onboard = async (req: NextApiRequest, res: NextApiResponse) => {
         card_issuing: {
           tos_acceptance: TOS_ACCEPTANCE,
         },
-        ...(useTreasury && {
+        // Embedded Finance is a full financial services stack for your users:
+        // accounts[0] with Treasury to store and send funds, with cards[1] with
+        // Issuing for spending.
+        // This is different from the Expense Management example, where you
+        // top up balances[2] to fund spend on Issuing cards.
+        //
+        // [0] https://stripe.com/docs/treasury/account-management/financial-accounts
+        // [1] https://stripe.com/docs/issuing/how-issuing-works
+        // [2] https://stripe.com/docs/issuing/adding-funds-to-your-card-program
+        ...(useCase == UseCase.EmbeddedFinance && {
           treasury: {
             tos_acceptance: TOS_ACCEPTANCE,
           },
