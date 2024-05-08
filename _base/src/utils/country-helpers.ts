2dd05e1b2c94336736c1820d222474eeb32bbba4
Kate Roumruk <kater@stripe.com>
2024-05-08 15:55:00 +0100

    amend render.yaml

--- _base/src/utils/country-helpers.ts
+++ _base/src/utils/country-helpers.ts
@@ -9,166 +9,166 @@ export function getCountryVars(country: string) {
     switch (country) {
 
         case 'AT': //Austria
-            phoneRegEx = '+43(#)### ####';
+            phoneRegEx = "+43(#)### ####";
             city = "Vienna";
             postcode = "1010"
             break;
 
         case 'AT': //Austria
-            phoneRegEx = '+43(#)### ####';
+            phoneRegEx = "+43(#)### ####";
             city = "Vienna";
             postcode = "1010"
             break;
 
         case 'BE': //Belgium
-            phoneRegEx = '+32(#)## ## ## ##';
+            phoneRegEx = "+32(#)## ## ## ##";
             city = "Brussels";
             postcode = "1000"
             break;
 
         // case 'Bulgaria':
-        //     phoneRegEx = '+359(#)### ####';
+        //     phoneRegEx = "+359(#)### ####";
         //     break;
 
         case 'HR': //Croatia
-            phoneRegEx = '+385(#)## ### ###';
+            phoneRegEx = "+385(#)## ### ###";
             city = "Zagreb";
             postcode = "10000"
             break;
 
         case 'CY': //Cyprus
-            phoneRegEx = '+357(#)## ### ###';
+            phoneRegEx = "+357(#)## ### ###";
             city = "Lefkosia";
             postcode = "1105"
             break;
         
         // case 'Czech Republic':
-        //     phoneRegEx = '+420(#)### ### ###';
+        //     phoneRegEx = "+420(#)### ### ###";
         //     break;
 
         // case 'Denmark':
-        //     phoneRegEx = '+45(#)## ## ## ##';
+        //     phoneRegEx = "+45(#)## ## ## ##";
         //     break;
 
         case 'EE': //Estonia
-            phoneRegEx = '+372(#)#### ####';
+            phoneRegEx = "+372(#)#### ####";
             city = "Tallinn";
             postcode = "10101"
             break;
 
         case 'FI': //Finland
-            phoneRegEx = '+358(#)## ### ####';
+            phoneRegEx = "+358(#)## ### ####";
             city = "Helsinki";
             postcode = "00100"
             break;
 
         case 'FR': //France
-            phoneRegEx = '+33(#)#########';
+            phoneRegEx = "+33(#)#########";
             city = "Paris";
             postcode = "75001"
             break;
 
         case 'DE': //Germany
-            phoneRegEx = '+49(#)#########';
+            phoneRegEx = "+49(#)#########";
             city = "Berlin";
             postcode = "10176"
             break;
 
         case 'GB': //Great Britain
-            phoneRegEx = '+44(#)#### #####';
+            phoneRegEx = "+44(#)#### #####";
             city = "London";
             postcode = "WC32 4AP";
             break;
 
         case 'GR': //Greece
-            phoneRegEx = '+30(#)### #### ###';
+            phoneRegEx = "+30(#)### #### ###";
             city = "Athens";
             postcode = "104 35";
             break;
 
         // case 'Hungary':
-        //     phoneRegEx = '+36(#)## ### ###';
+        //     phoneRegEx = "+36(#)## ### ###";
         //     break;
 
         case 'IE': //Ireland
-            phoneRegEx = '+353(#)## ### ####';
+            phoneRegEx = "+353(#)## ### ####";
             city = "Dublin";
             postcode = "D01 B2CD";
             break;
 
         case 'IT': //Italy
-            phoneRegEx = '+39(#)#########';
+            phoneRegEx = "+39(#)#########";
             city = "Rome";
             postcode = "00042";
             break;
 
         case 'LV': //Latvia
-            phoneRegEx = '+371(#)## ######';
+            phoneRegEx = "+371(#)## ######";
             city = "Riga";
             postcode = "1000";
             break;
 
         case 'LT': //Lithuania
-            phoneRegEx = '+370(#)## ######';
+            phoneRegEx = "+370(#)## ######";
             city = "Vilnius";
             postcode = "01100";
             break;
 
         case 'LU': //Luxembourg
-            phoneRegEx = '+352(#)#### ####';
+            phoneRegEx = "+352 (#)#### ####";
             city = "Luxembourg City";
             postcode = "1111";
             break;
 
         case 'MT': //Malta
-            phoneRegEx = '+356(#)#### ####';
+            phoneRegEx = "+356(#)#### ####";
             city = "Valletta";
             postcode = "VLT 1061";
             break;
 
         case 'NL': //Netherlands
-            phoneRegEx = '+31(#)## ########';
+            phoneRegEx = "+31(#)## ########";
             city = "Amsterdam";
             postcode = "1008 DG";
             break;
 
         // case 'Poland':
-        //     phoneRegEx = '+48(#)### ### ###';
+        //     phoneRegEx = "+48(#)### ### ###";
         //     break;
 
         case 'PT': //Portugal
-            phoneRegEx = '+351(#)### ### ###';
+            phoneRegEx = "+351(#)### ### ###";
             city = "Lisbon";
             postcode = "1000";
             break;
 
         // case 'Romania':
-        //     phoneRegEx = '+40(#)### ### ###';
+        //     phoneRegEx = "+40(#)### ### ###";
         //     break;
 
         case 'SK': //Slovakia
-            phoneRegEx = '+421(#)### ### ###';
+            phoneRegEx = "+421(#)### ### ###";
             city = "Bratislava";
             postcode = "2412";
             break;
 
         case 'SI': //Slovenia
-            phoneRegEx = '+386(#)## ### ###';
+            phoneRegEx = "+386(#)## ### ###";
             city = "Ljubljana";
             postcode = "1000";
             break;
 
         case 'ES': //Spain
-            phoneRegEx = '+34(6)## ## ## ##';
+            phoneRegEx = "+34(6)## ## ## ##";
             city = "Barcelona";
             postcode = "08001";
             break;
 
         // case 'Sweden':
-        //     phoneRegEx = '+46(#)## ### ## ##';
+        //     phoneRegEx = "+46(#)## ### ## ##";
         //     break;
     };
-    
+
     let countryVars = {
         phone: faker.phone.number(faker.helpers.fromRegExp(phoneRegEx)),
         city: city,
